79277e18ddabfbf02bb3cdfefeea64535a687f22
vishaldiwase <vishal.diwase@yahoo.com>
2025-08-27 22:01:52 +0200

    new file added

---
+++ currency.py
@@ -0,0 +1,7 @@
+# Write code below 💖
+pesos=float(input('What do you have left in pesos?:'))
+soles=float(input('What do you have left in soles?:'))
+reais=float(input('What do you have left in reais?:'))
+
+USD= (pesos*0.00025)+(soles*0.28)+(reais*0.18)
+print(USD)
